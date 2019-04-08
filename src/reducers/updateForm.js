98b4f5c8fc34224f4f572ff2f0cf6634fd966243
robotic-monkey <seanpatmiller@gmail.com>
2019-04-07 20:09:38 -0800

    4719 update

--- src/reducers/updateForm.js
+++ src/reducers/updateForm.js
@@ -18,7 +18,7 @@ export default (state = testerReducerDefaultState, action) => {
         }
 
         if (index === 5 && sel[index].includes("X")) {
-          selectionArray.push({slot:sel[index+1].slice(1), code:sel[index],team: "Traya L" })
+          selectionArray.push({slot:sel[index+1].slice(1), code:sel[index],team: "Traya + BSF (if geared)" })
         }
 
         if (index === 7 && sel[index].includes("X")) {
